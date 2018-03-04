953416b10abc02dd1e6595e9ec1f26cbfca31a14
Chuck J Hardy <chuckjhardy@gmail.com>
2018-03-04 07:39:44 +0000

    Segue from settings to Dashboard

--- Wemotely/Controllers/Settings/NotificationOptionsTableViewController.swift
+++ Wemotely/Controllers/Settings/NotificationOptionsTableViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class NotificationOptionsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Notification Options"
         navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
         navigationController?.setToolbarHidden(true, animated: false)
     }
--- Wemotely/Controllers/SettingsTableViewController.swift
+++ Wemotely/Controllers/SettingsTableViewController.swift
@@ -5,4 +5,8 @@ class SettingsTableViewController: UITableViewController {
         super.viewDidLoad()
         navigationController?.setToolbarHidden(true, animated: false)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        performSegue(withIdentifier: "reshowDetailView", sender: self)
+    }
 }

2ff888009f83a77ac3c3b477000624d93c4f1d3a
Chuck J Hardy <chuckjhardy@gmail.com>
2018-03-08 18:47:19 +0000

    Basic DAO Object

---
+++ WemotelyTests/Services/FeedService+DAOTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+
+@testable import Wemotely
+
+class FeedService_DAOTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testDefaults() {
+        
+    }
+}

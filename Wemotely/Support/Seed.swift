a2085645a2ea56321062051de1390f95e31bf3e3
Chuck J Hardy <chuckjhardy@gmail.com>
2018-03-09 08:32:01 +0000

    Account Seed

--- Wemotely/Support/Seed.swift
+++ Wemotely/Support/Seed.swift
@@ -1,9 +1,107 @@
-//
-//  Seed.swift
-//  Wemotely
-//
-//  Created by Chuck J Hardy on 09/03/2018.
-//  Copyright © 2018 Insert Coffee Limited. All rights reserved.
-//
-
 import Foundation
+import RealmSwift
+
+class Seed: NSObject {
+    var realm: Realm
+    var app: App!
+    
+    init(realm: Realm = RealmProvider.realm()) {
+        self.realm = realm
+    }
+    
+    enum Categories: String {
+        case programming = "Programming"
+        case customerSupport = "Customer Support"
+        case devops = "Devops"
+        case marketing = "Marketing"
+        case business = "Business/Management"
+        case copywriting = "Copywriting"
+    }
+    
+    func call() {
+        if let existingApp = realm.objects(App.self).first {
+            app = existingApp
+        } else {
+            app = App()
+        }
+        
+        if !app.seeded {
+            for account in accounts() {
+                app.accounts.append(account)
+            }
+            
+            app.seeded = true
+            
+            try! realm.write {
+                realm.add(app, update: true)
+            }
+        }
+    }
+    
+    func accounts() -> [Account] {
+        return [
+            Account(
+                value: [
+                    "title": Categories.programming.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey": "remote-programming-jobs",
+                    "order": 0
+                ]
+            ),
+            Account(
+                value: [
+                    "title": Categories.customerSupport.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey": "remote-customer-support-jobs",
+                    "order": 1
+                ]
+            ),
+            Account(
+                value: [
+                    "title": Categories.devops.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey": "remote-devops-sysadmin-jobs",
+                    "order": 2
+                ]
+            ),
+            Account(
+                value: [
+                    "title": Categories.marketing.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey": "remote-marketing-jobs",
+                    "order": 3
+                ]
+            ),
+            Account(
+                value: [
+                    "title": Categories.business.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey": "remote-business-exec-management-jobs",
+                    "order": 4
+                ]
+            ),
+            Account(
+                value: [
+                    "title": Categories.copywriting.rawValue,
+                    "icon": "inbox",
+                    "unreadCount": 0,
+                    "active": true,
+                    "urlKey":
+                    "remote-copywriting-jobs",
+                    "order": 5
+                ]
+            )
+        ]
+    }
+}
+
---
+++ WemotelyTests/Support/SeedTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+
+@testable import Wemotely
+
+class SeedTests: XCTestCase {
+    let realm = RealmProvider.realm()
+    
+    override func setUp() {
+        super.setUp()
+        
+        try! realm.write { () -> Void in
+            realm.deleteAll()
+        }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testAlreadySeeded() {
+        let app = App()
+        app.seeded = true
+        
+        try! realm.write {
+            realm.add(app, update: true)
+        }
+        
+        Seed(realm: realm).call()
+        
+        XCTAssertEqual(realm.objects(App.self).count, 1)
+        XCTAssertEqual(realm.objects(App.self).first?.accounts.count, 0)
+    }
+    
+    func testUnseeded() {
+        Seed().call()
+        
+        XCTAssertEqual(realm.objects(App.self).count, 1)
+        XCTAssertEqual(realm.objects(App.self).first?.accounts.count, 6)
+    }
+}

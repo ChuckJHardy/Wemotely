44feda6f88f677a022c62d2a363858b8baf8ff97
Chuck J Hardy <chuckjhardy@gmail.com>
2018-04-03 11:02:55 +0100

    Update Account Spec

--- WemotelyTests/Models/Account+QueryTests.swift
+++ WemotelyTests/Models/Account+QueryTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import RealmSwift
 
 @testable import Wemotely
 
@@ -9,6 +10,87 @@ class AccountQueryTests: BaseTestCase {
 
     override func tearDown() {
         super.tearDown()
+    }
+
+    func testByUUIDReturningAccount() {
+        let realm = RealmProvider.realm()
+        let app = App()
+
+        let account1 = Account(value: ["urlKey": "A"])
+        let account2 = Account(value: ["urlKey": "B"])
+
+        app.accounts.append(account1)
+        app.accounts.append(account2)
+
+        XCTAssertEqual(realm.objects(Account.self).count, 0)
+
+        // swiftlint:disable:next force_try
+        try! realm.write {
+            realm.add(app)
+        }
+
+        XCTAssertEqual(realm.objects(Account.self).count, 2)
+
+        let sut = Account.byUUID(provider: realm, uuid: account1.uuid)
+
+        XCTAssertEqual(sut?.uuid, account1.uuid)
+    }
+
+    func testByUUIDReturningAccounts() {
+        let realm = RealmProvider.realm()
+        let app = App()
+
+        let account1 = Account(value: ["urlKey": "A"])
+        let account2 = Account(value: ["urlKey": "B"])
+        let account3 = Account(value: ["urlKey": "C"])
+
+        app.accounts.append(account1)
+        app.accounts.append(account2)
+        app.accounts.append(account3)
+
+        XCTAssertEqual(realm.objects(Account.self).count, 0)
+
+        // swiftlint:disable:next force_try
+        try! realm.write {
+            realm.add(app)
+        }
+
+        XCTAssertEqual(realm.objects(Account.self).count, 3)
+
+        let sut = Account.byUUID(provider: realm, uuids: [account1.uuid, account2.uuid])
+
+        XCTAssertEqual(sut?.first?.uuid, account1.uuid)
+        XCTAssertEqual(sut?.last?.uuid, account2.uuid)
+        XCTAssertFalse((sut?.contains(account3))!)
+    }
+
+    func testRefreshable() {
+        let realm = RealmProvider.realm()
+        let app = App()
+
+        let account1 = Account(value: ["urlKey": "A"])
+        let account2 = Account(value: ["urlKey": "B"])
+
+        app.accounts.append(account1)
+        app.accounts.append(account2)
+
+        XCTAssertEqual(realm.objects(Account.self).count, 0)
+
+        // swiftlint:disable:next force_try
+        try! realm.write {
+            realm.add(app)
+        }
+
+        XCTAssertEqual(realm.objects(Account.self).count, 2)
+
+        let sut = Account.refreshable(provider: realm, uuid: account1.uuid)
+
+        XCTAssertEqual(sut?.count, 1)
+        XCTAssertEqual(sut?.first?.uuid, account1.uuid)
+    }
+
+    func testRefreshableWithoutUUID() {
+        activeSortedShared(sut: Account.refreshable(provider: realm)!)
     }
 
     func testAllSorted() {
@@ -40,6 +122,10 @@ class AccountQueryTests: BaseTestCase {
     }
 
     func testActiveSorted() {
+        activeSortedShared(sut: Account.activeSorted(provider: realm))
+    }
+
+    private func activeSortedShared(sut: Results<Account>) {
         let realm = RealmProvider.realm()
         let app = App()
 
@@ -59,9 +145,6 @@ class AccountQueryTests: BaseTestCase {
         }
 
         XCTAssertEqual(realm.objects(Account.self).count, 3)
-
-        let sut = Account.activeSorted(provider: realm)
-
         XCTAssertEqual(sut.count, 2)
         XCTAssertEqual(sut.first, account2)
         XCTAssertEqual(sut.last, account1)

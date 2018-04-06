d7f20d9e4dabca5cd03f03f248c84d6c6954031d
Chuck J Hardy <chuckjhardy@gmail.com>
2018-04-06 12:28:54 +0100

    Updated Seed Specs

--- WemotelyTests/Support/SeedTests.swift
+++ WemotelyTests/Support/SeedTests.swift
@@ -3,8 +3,16 @@ import XCTest
 @testable import Wemotely
 
 class SeedTests: BaseTestCase {
+    var beforeTriggered: Bool!
+    var afterTriggered: Bool!
+    var skippedTriggered: Bool!
+
     override func setUp() {
         super.setUp()
+
+        beforeTriggered = false
+        afterTriggered = false
+        skippedTriggered = false
     }
 
     override func tearDown() {
@@ -21,27 +29,35 @@ class SeedTests: BaseTestCase {
         }
 
         Seed(realm: realm).call(before: {
-            // Nothing
+            beforeTriggered = true
         }, after: {
-            // Nothing
+            afterTriggered = true
         }, skipped: {
-            // Nothing
+            skippedTriggered = true
         })
 
         XCTAssertEqual(realm.objects(App.self).count, 1)
         XCTAssertEqual(realm.objects(App.self).first?.accounts.count, 0)
+
+        XCTAssertFalse(beforeTriggered)
+        XCTAssertFalse(afterTriggered)
+        XCTAssertTrue(skippedTriggered)
     }
 
     func testUnseeded() {
         Seed(realm: realm).call(before: {
-            // Nothing
+            beforeTriggered = true
         }, after: {
-            // Nothing
+            afterTriggered = true
         }, skipped: {
-            // Nothing
+            skippedTriggered = true
         })
 
         XCTAssertEqual(realm.objects(App.self).count, 1)
         XCTAssertEqual(realm.objects(App.self).first?.accounts.count, 6)
+
+        XCTAssertTrue(beforeTriggered)
+        XCTAssertTrue(afterTriggered)
+        XCTAssertFalse(skippedTriggered)
     }
 }

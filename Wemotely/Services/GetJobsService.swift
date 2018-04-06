5bcc2a7abfc0ed332edb35150a266cb5df88846e
Chuck J Hardy <chuckjhardy@gmail.com>
2018-04-06 18:29:19 +0100

    Get Job Service

---
+++ Wemotely/Services/GetJobsService.swift
@@ -0,0 +1,9 @@
+//
+//  GetJobsService.swift
+//  Wemotely
+//
+//  Created by Chuck J Hardy on 06/04/2018.
+//  Copyright © 2018 Insert Coffee Limited. All rights reserved.
+//
+
+import Foundation

a3c5381162b216867c151b1a91103bdd2b7b957c
Chuck J Hardy <chuckjhardy@gmail.com>
2018-04-15 15:45:20 +0100

    Job Presenter

--- Wemotely/Presenters/JobsPresenter.swift
+++ Wemotely/Presenters/JobsPresenter.swift
@@ -1,22 +1,7 @@
 import Foundation
 
-class JobsPresenter: NSObject {
-    var locale: Locale!
-
-    init(locale: Locale? = Locale.current) {
-        self.locale = locale
-    }
-
+class JobsPresenter: BasePresenter {
     func navigationPrompt(date: Date? = nil) -> String {
-        if let validDate = date, let formattedDate = formatDate(date: validDate) {
-            return "Updated \(formattedDate.timeAgo())"
-        }
-
-        return ""
-    }
-
-    private func formatDate(date: Date) -> Date? {
-        let component = Calendar.current.dateComponents(in: TimeZone.current, from: date)
-        return component.date
+        return dateAsString(prefix: "Updated", date: date)
     }
 }

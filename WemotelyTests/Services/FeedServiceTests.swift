541a7cb381f1b59b5fa2fc3fdc155c4bde90667b
Chuck J Hardy <chuckjhardy@gmail.com>
2018-04-17 09:02:57 +0100

    Feed Service should Create and Update Jobs

--- WemotelyTests/Services/FeedServiceTests.swift
+++ WemotelyTests/Services/FeedServiceTests.swift
@@ -4,35 +4,31 @@ import SwiftHash
 @testable import Wemotely
 
 class FeedServiceTests: BaseTestCase {
-    override func setUp() {
-        super.setUp()
-    }
-
-    override func tearDown() {
-        super.tearDown()
-    }
-
     func testE2E() {
         let account = Account()
-        let url = URLProvider(key: "remote-programming-jobs").url()
+        let url = URLProviderFactory().build(key: "remote-programming-jobs")
         let testDate = Date(timeInterval: 1000, since: Date())
-        let feedService = FeedService(account: account, updatedAt: testDate)
+        let feedService = FeedService(
+            provider: realm,
+            account: account,
+            updatedAt: testDate
+        )
 
         XCTAssertNil(account.lastUpdated)
 
         if let result = feedService.parser(url: url)?.parse() {
-            feedService.save(realm: realm, feed: result.rssFeed!)
+            feedService.save(feed: result.rssFeed!)
 
             XCTAssertEqual(account.jobs.count, 2)
 
             let firstJob = account.jobs.first
             XCTAssertEqual(firstJob?.account?.uuid, account.uuid)
-            XCTAssertEqual(firstJob?.title, "Full Stack Dev with Rails Focus")
-            XCTAssertEqual(firstJob?.company, "NuRelm, Inc.")
-            XCTAssertEqual(firstJob?.body.count, 4826)
+            XCTAssertEqual(firstJob?.title, "Awesome Job Titles")
+            XCTAssertEqual(firstJob?.company, "A Company")
+            XCTAssertEqual(firstJob?.body.count, 11)
             XCTAssertEqual(
                 firstJob?.guid,
-                MD5("https://weworkremotely.com/remote-jobs/nurelm-inc-full-stack-dev-with-rails-focus")
+                MD5("https://weworkremotely.com/remote-jobs/nurelm-inc-full-stack-dev-with-rails-focus-other")
             )
             XCTAssertEqual(
                 firstJob?.link,
